e97604efb397e51ba0db307d27b61c8a3fab29e3
definstall <119135414+definstall@users.noreply.github.com>
2025-08-15 19:14:22 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -14,7 +14,7 @@ urls=(
     "https://git.io/oneclick  trojan_v2ray脚本"
     "https://raw.githubusercontent.com/xykt/IPQuality/main/ip.sh IP质量检测脚本"
     "https://raw.githubusercontent.com/localpoliy/install/local/bbr.sh BBR_脚本"
-    "https://raw.githubusercontent.com/yonggekkk/sing-box-yg/main/sb.sh 一键四协议"
+    "https://raw.githubusercontent.com/alireza0/s-ui/1.2.2/install.sh 1.2.2S-UI"
     "===================="
     "https://raw.githubusercontent.com/definstall/Definition/local/ssh.sh SSH脚本"
     "https://raw.githubusercontent.com/definstall/Definition/local/fail2ban.sh Fail2ban脚本"
