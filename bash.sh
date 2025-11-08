5e7fb85bd608cde53b5849bd6b364a946b9b564e
definstall <119135414+definstall@users.noreply.github.com>
2025-11-08 15:57:52 +0800

    Remove Ubuntu upgrade script from bash.sh
    Removed the upgrade script for Ubuntu from the list.

--- bash.sh
+++ bash.sh
@@ -25,7 +25,6 @@ urls=(
     "https://raw.githubusercontent.com/definstall/Definition/local/xrdps.sh Kubuntu桌面"
     "https://raw.githubusercontent.com/definstall/Definition/local/docker.sh Docker_脚本"
     "===================="
-    "https://raw.githubusercontent.com/definstall/Definition/local/upgrade_to_jammy.sh ubuntu升级脚本"
     "https://raw.githubusercontent.com/definstall/ssh_tool/main/ssh_tool.sh -o ssh_tool.sh Tools脚本"
     "https://raw.githubusercontent.com/localpoliy/install/local/wordpress_redis.sh docker_WordPress_Redis_脚本"
     "退出脚本"
