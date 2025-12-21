dedb0c722621df750001fea9590f94f6bd9d416f
definstall <119135414+definstall@users.noreply.github.com>
2025-12-22 01:34:13 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -11,7 +11,6 @@ echo
 
 # Array of URLs with corresponding descriptions
 urls=(
-    "https://git.io/oneclick  trojan_v2ray脚本"
     "https://raw.githubusercontent.com/xykt/IPQuality/main/ip.sh IP质量检测脚本"
     "https://raw.githubusercontent.com/localpoliy/install/local/bbr.sh BBR_脚本"
     "https://raw.githubusercontent.com/alireza0/s-ui/1.2.2/install.sh 1.2.2S-UI"
