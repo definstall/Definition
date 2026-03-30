b63fad1db8be7c4a21b2fb1f6332aac440f6f443
definstall <119135414+definstall@users.noreply.github.com>
2026-03-30 14:24:54 +0800

    Update xrdps script link description

--- bash.sh
+++ bash.sh
@@ -22,7 +22,7 @@ urls=(
     "https://raw.githubusercontent.com/definstall/Definition/local/frp.sh Frp内网-穿透"
     "===================="
     "https://raw.githubusercontent.com/definstall/Definition/local/nginx.sh Nginx_脚本"
-    "https://raw.githubusercontent.com/definstall/Definition/local/xrdps.sh Kubuntu桌面"
+    "https://raw.githubusercontent.com/definstall/Definition/local/xrdps.sh Ubuntu升级/桌面"
     "https://raw.githubusercontent.com/definstall/Definition/local/docker.sh Docker_脚本"
     "===================="
     "https://raw.githubusercontent.com/definstall/ssh_tool/main/ssh_tool.sh -o ssh_tool.sh Tools脚本"
